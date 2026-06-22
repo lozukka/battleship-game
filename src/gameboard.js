3a761e008ce340e0f9b6a3fdb032221666109cb6
Lotta <lotsulotsu@gmail.com>
2026-06-22 09:10:39 +0300

    place ship hor or ver

--- src/gameboard.js
+++ src/gameboard.js
@@ -5,6 +5,26 @@ export const Gameboard = () => {
 
   function placeShip(length, name, startCoord, direction) {
     const ship = Ship(length, name);
+    const placedCoord = [[startCoord]];
+    const placed = new Set();
+    placed.add(positionKey(startCoord));
+
+    if (direction === "horizontally") {
+      for (let i = 0; i < length.length; i++) {
+        const newCoord = [];
+        newCoord.push(startCoord[0] + 1, startCoord[1]);
+      }
+      return newCoord;
+    } else {
+      for (let i = 0; i < length.length; i++) {
+        const newCoord = [];
+        newCoord.push(startCoord[0], startCoord[1] + 1);
+      }
+      return newCoord;
+    }
+  }
+  function positionKey([x, y]) {
+    return `${x}, ${y}`;
   }
   return { board };
 };
