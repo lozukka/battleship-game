3e3d3329c25fc2af1e0a4e487dad1dbf978b9c44
Lotta <lotsulotsu@gmail.com>
2026-06-22 08:53:00 +0300

    create a empty gameboard

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,4 +1,10 @@
+import { Ship } from "../src/ship";
+
 export const Gameboard = () => {
-  let emptyBoard = [];
-  return emptyBoard;
+  let board = Array.from({ length: 10 }, () => Array(10).fill(null));
+
+  function placeShip(length, name, startCoord, direction) {
+    const ship = Ship(length, name);
+  }
+  return { board };
 };
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -3,6 +3,19 @@ import { Gameboard } from "../src/gameboard";
 describe("gameboard", () => {
   test("create an empty gameboard", () => {
     const emptyBoard = Gameboard();
-    expect(emptyBoard.length).toBe(0);
+    expect(emptyBoard.board).toStrictEqual([
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+    ]);
   });
+
+  test("place ship horizontally or vertically from a starting coordinate", () => {});
 });
