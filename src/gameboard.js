1a2aebfcab7ebfdf3937faa35a2e57607916fe98
Lotta <lotsulotsu@gmail.com>
2026-06-24 19:12:19 +0300

    simplifyed placeship function for ship placement

--- src/gameboard.js
+++ src/gameboard.js
@@ -3,26 +3,28 @@ import { Ship } from "../src/ship";
 export const Gameboard = () => {
   let board = Array.from({ length: 10 }, () => Array(10).fill(null));
 
+  const shipCoords = new Map();
+
   function placeShip(length, name, startCoord, direction) {
     const ship = Ship(length, name);
-    const placedCoord = [[startCoord]];
-    const placed = new Set();
-    placed.add(positionKey(startCoord));
+    const coords = [];
 
-    if (direction === "horizontally") {
-      for (let i = 0; i < length.length; i++) {
-        const newCoord = [];
-        newCoord.push(startCoord[0] + 1, startCoord[1]);
-      }
-      return newCoord;
-    } else {
-      for (let i = 0; i < length.length; i++) {
-        const newCoord = [];
-        newCoord.push(startCoord[0], startCoord[1] + 1);
-      }
-      return newCoord;
+    for (let i = 0; i < length; i++) {
+      const coord =
+        direction === "horizontal"
+          ? [startCoord[0] + i, startCoord[1]]
+          : [startCoord[0], startCoord[1] + i];
+
+      coords.push(coord);
+    }
+    //validation
+    for (const coord of coords) {
+      const key = positionKey(coord);
+      shipCoords.set(key, ship);
+      board[coord[1]][coord[0]] = ship;
     }
   }
+
   function positionKey([x, y]) {
     return `${x}, ${y}`;
   }
