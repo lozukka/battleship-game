57632fd1ed76957483e3f32e786c98cec2cd4a7a
Lotta <lotsulotsu@gmail.com>
2026-08-31 09:12:51 +0300

    fixing the ghpage rendering

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,12 +2,13 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+    publicPath: "https://github.com/lozukka/battleship-game",
   },
   plugins: [
     new HtmlWebpackPlugin({
